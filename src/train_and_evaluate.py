c47c16ad8a607620f6c49719b688d0bf48176ac3
Sahil Vaishnav <sahilvaishnav94@gmail.com>
2021-06-18 19:08:44 +0530

    stage 3 complete now

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -51,18 +51,19 @@ def train_and_evaluate(config_path):
     
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-    
+    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
 
+    os.makedirs(model_dir, exist_ok=True)
+    model_path = os.path.join(model_dir, "model.joblib")
 
-
-
-
-
-
+    joblib.dump(lr,model_path)
 
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    split_and_saved_data(config_path=parsed_args.config)
+    train_and_evaluate(config_path=parsed_args.config)
